27701fed12d03bf682d05368516c855832f04379
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2025-05-21 15:29:08 +0530

    added address repo and updated other repo

--- sb-ecom/src/main/java/com/ecommerce/project/repositories/AddressRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/project/repositories/AddressRepository.java
@@ -1,4 +1,8 @@
 package com.ecommerce.project.repositories;
 
-public class AddressRepository {
+import com.ecommerce.project.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
