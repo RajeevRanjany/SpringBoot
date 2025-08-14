b1462b6913814c57f825d869595f27eee3caf3f5
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2025-08-15 02:04:54 +0530

    ulitlity services

--- MovieBookingApplication/src/main/java/com/example/MovieBookingApplication/dto/BookingDTO.java
+++ MovieBookingApplication/src/main/java/com/example/MovieBookingApplication/dto/BookingDTO.java
@@ -1,6 +1,7 @@
 package com.example.MovieBookingApplication.dto;
 
 import com.example.MovieBookingApplication.entity.BookingStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 public class BookingDTO {
+    @Schema(name = "No of seats You want to book", example = "1/2/3")
     private Integer numberOfSeats;
     private LocalDateTime bookingTime;
     private double price;
